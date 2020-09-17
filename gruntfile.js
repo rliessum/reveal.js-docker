6dcab4120a6aa33d112076535622fdbd1ec0ba61
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2020-09-17 16:19:45 +0200

    Watch for MD files recursively

--- gruntfile.js
+++ gruntfile.js
@@ -146,7 +146,7 @@ module.exports = grunt => {
 					'lib/**',
 					'images/**',
 					'plugin/**',
-					'docs/slides/**.md',
+					'docs/slides/**/**.md',
                     'favicon.ico'
 				],
 				dest: 'reveal-js-presentation.zip'
@@ -180,7 +180,7 @@ module.exports = grunt => {
 			},
 			markdown: {
 				files: [
-					'docs/slides/**.md',
+					'docs/slides/**/**.md',
 				]
 			},
 			options: {
